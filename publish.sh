144954fec8633d5598e83b2f4dd64004b9e16ac2
Chris Putnam <chris@webfx.com>
2022-06-16 07:54:32 -0400

    Publishing - betas

--- publish.sh
+++ publish.sh
@@ -7,23 +7,23 @@ EXTENSION_NAME="$(basename "$EXTENSION_FOLDER")"
 # Package for Firefox first
 rm -f "$HOME/Downloads/$EXTENSION_NAME-firefox.zip"
 cd "$EXTENSION_FOLDER" || exit 1
-mv manifest.json manifest-3.0.json
-mv manifest-2.0.json manifest.json
+mv manifest.json manifest-google.json
+mv manifest-firefox.json manifest.json
 zip -r "$HOME/Downloads/$EXTENSION_NAME-firefox.zip" . \
 	--exclude ".*" \
-	--exclude "manifest-2.0.json" \
-	--exclude "manifest-3.0.json" \
+	--exclude "manifest-firefox.json" \
+	--exclude "manifest-google.json" \
 	--exclude "publish.sh" \
 	-q
 
 # Package for Chrome
 rm -f "$HOME/Downloads/$EXTENSION_NAME-chrome.zip"
-mv manifest.json manifest-2.0.json
-mv manifest-3.0.json manifest.json
+mv manifest.json manifest-firefox.json
+mv manifest-google.json manifest.json
 cd ..
 zip -r "$HOME/Downloads/$EXTENSION_NAME-chrome.zip" "$EXTENSION_NAME" \
 	--exclude "$EXTENSION_NAME/.*" \
-	--exclude "$EXTENSION_NAME/manifest-2.0.json" \
-	--exclude "$EXTENSION_NAME/manifest-3.0.json" \
+	--exclude "$EXTENSION_NAME/manifest-firefox.json" \
+	--exclude "$EXTENSION_NAME/manifest-google.json" \
 	--exclude "$EXTENSION_NAME/publish.sh" \
 	-q
