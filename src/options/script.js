0d289c2d9f3e6e74126bb9e8deb3f98b9cda89be
Chris Putnam <chris@webfx.com>
2024-02-16 16:06:04 -0500

    Refactoring to use storage for options

--- src/options/script.js
+++ src/options/script.js
@@ -1,32 +1,13 @@
-const root = document.querySelector(':root');
-const body = document.querySelector('body');
+/* global chrome */
 
-/* Initialize inputs */
-document.querySelectorAll('input').forEach((input) => {
-	const variable = `--setting_${input.name}`;
-
-	/* Set initial value */
-	input.value = getComputedStyle(root).getPropertyValue(variable);
-
-	/* Listen for changes */
-	input.addEventListener('change', function () {
-		// Validate int values with min/max
-		let new_value = parseInt(this.value);
-		if (new_value < this.min) {
-			new_value = this.min;
-		}
-		if (new_value > this.max) {
-			new_value = this.max;
-		}
-		this.value = new_value;
-
-		if (this.name === 'note_size') {
-			body.classList.toggle(
-				'gkfs-fullscreen-custom-size',
-				new_value < 100
-			);
-		}
-
-		root.style.setProperty(variable, new_value);
+/* Listen for form submit and update settings */
+document.querySelector('form').addEventListener('submit', function (event) {
+	event.preventDefault();
+	const settings = {};
+	this.querySelectorAll('input[name]').forEach((input) => {
+		settings[input.name] = parseInt(input.value);
+	});
+	chrome.storage.sync.set({
+		settings: settings,
 	});
 });
---
+++ src/settings_listener.js
@@ -0,0 +1,33 @@
+/* global chrome */
+const root = document.querySelector(':root');
+const body = document.querySelector('body');
+
+/* Listen for messages to update settings */
+const options = ['note_size', 'note_padding', 'backdrop_opacity'];
+chrome.runtime.onMessage.addListener(function (request) {
+	console.log(request);
+
+	if ('settings' in request) {
+		options.forEach((option) => {
+			const variable = `--setting_${option}`;
+			const new_value = request.settings[option];
+			if (option === 'note_size') {
+				body.classList.toggle(
+					'gkfs-fullscreen-custom-size',
+					new_value < 100
+				);
+			}
+			root.style.setProperty(variable, request.settings[input.name]);
+			const input = document.querySelector(`input[name="${option}"]`);
+			if (input) {
+				input.value = new_value;
+			}
+		});
+	}
+});
+
+function updateSettings(settings) {
+	document.querySelectorAll('input[name]').forEach((input) => {
+		input.value = settings[input.name];
+	});
+}
